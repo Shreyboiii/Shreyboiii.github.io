50b61300840c5869e3b81861bad7c3ee07bb7d18
Shreyboiii <shreyash.hamal@gmail.com>
2024-03-11 17:28:23 -0700

    saving

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
+const dynamicText = document.querySelector("p span");
+const sentences = ["An Aspiring Developer", "Nice To Meet You"];
+let sentenceIndex = 0;
+let charIndex = 0; 
+let isDeleting = false;
+typing();
+
 function toggleMenu(){
     const menu = document.querySelector(".ham-links");
     const icon = document.querySelector(".hamburger-icon");
 
     menu.classList.toggle("open");
     icon.classList.toggle("open");
+}
 
+function typing(){
+    let currentSentence = sentences[sentenceIndex];
+    let currentChar = currentSentence.substring(0,charIndex);
+    dynamicText.innerHTML = currentChar;
+    console.log(dynamicText.innerHTML);
 
+    if(!isDeleting && charIndex < currentSentence.length) {
+        charIndex++;
+        setTimeout(typing,100);
+    } else if (isDeleting && charIndex > 0){
+        charIndex--;
+        setTimeout(typing,50);
+    } else {
+        isDeleting = !isDeleting;
+        sentenceIndex = !isDeleting ? (sentenceIndex + 1) % sentences.length : sentenceIndex;
+        setTimeout(typing,200);
+    }
 }
