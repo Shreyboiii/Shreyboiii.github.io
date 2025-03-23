97e389908b4e4abcf67ffec9f9bdfa876051bbd9
Shreyboiii <shreyash.hamal@gmail.com>
2025-03-22 21:30:17 -0700

    Fixing content type

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,8 @@ app.use(express.json());
 app.post('/instagram-token', async (req, res) => {
   console.log('Received request at /instagram-token');
   res.setHeader('Access-Control-Allow-Origin', 'https://shreyboiii.github.io');
+  console.log('Content-Type:', req.headers['content-type']);
+  console.log(req.body);
   console.log('Code received:', req.body.code);
   try {
     const response = await fetch('https://api.instagram.com/oauth/access_token', {
