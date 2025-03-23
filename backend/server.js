4f03cf91603f918de2e3ca8c04aeb544bac2de7a
Shreyboiii <shreyash.hamal@gmail.com>
2025-03-22 21:14:51 -0700

    Using backend server completely

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,33 @@
 import fetch from 'node-fetch';
 import express from 'express';
+import { URLSearchParams } from 'url';
 const app = express();
 
 app.use(express.json());
 
 app.post('/instagram-token', async (req, res) => {
-  const response = await fetch('https://api.instagram.com/oauth/access_token', {
-    method: 'POST',
-    body: new URLSearchParams({
-      client_id: '1153807892876290',
-      client_secret: '9b84f2f7aa39bd5e6618ab014fd13365',
-      grant_type: 'authorization_code',
-      redirect_uri: 'https://shreyboiii.github.io/igdata',
-      code: req.body.code
-    })
-  });
-  const data = await response.json();
-  res.json(data);
+  console.log('Received request at /instagram-token');
+  res.setHeader('Access-Control-Allow-Origin', 'https://shreyboiii.github.io');
+  console.log('Code received:', req.body.code);
+  try {
+    const response = await fetch('https://api.instagram.com/oauth/access_token', {
+      method: 'POST',
+      body: new URLSearchParams({
+        client_id: '1153807892876290',
+        client_secret: '9b84f2f7aa39bd5e6618ab014fd13365',
+        grant_type: 'authorization_code',
+        redirect_uri: 'https://shreyboiii.github.io/igdata',
+        code: req.body.code
+      })
+    });
+    const data = await response.json();
+    console.log('Access token received:', data);
+    res.json(data);
+  } catch (error) {
+    console.error('Error fetching access token:', error);
+    res.status(500).json({ error: 'Failed to fetch access token' });
+  }
 });
 
+console.log('Listening at port 3000');
 app.listen(3000);
