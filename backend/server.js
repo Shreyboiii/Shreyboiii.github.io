5de92d82a1a2155ca0a4f6d0755b3cd78ca71747
Shreyboiii <shreyash.hamal@gmail.com>
2025-03-22 20:54:29 -0700

    Adding node server

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const fetch = require('node-fetch');
+const app = express();
+
+app.use(express.json());
+
+app.post('/instagram-token', async (req, res) => {
+  const response = await fetch('https://api.instagram.com/oauth/access_token', {
+    method: 'POST',
+    body: new URLSearchParams({
+      client_id: '1153807892876290',
+      client_secret: '9b84f2f7aa39bd5e6618ab014fd13365',
+      grant_type: 'authorization_code',
+      redirect_uri: 'https://shreyboiii.github.io/igdata',
+      code: req.body.code
+    })
+  });
+  const data = await response.json();
+  res.json(data);
+});
+
+app.listen(3000);
